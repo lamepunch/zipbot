438b1b3e7fdcff8cd274b4a7ee239942d7630424
Spencer Summerville <me@grenuttag.com>
2021-01-12 23:13:26 -0500

    Invocation counts are now properly tracked

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,36 @@
+import { readFile, writeFile } from "fs/promises"
 import { Client, Message, MessageEmbed } from "discord.js";
+
 import reactions from "./reactions.js";
 
 let client: Client = new Client();
 let count: number = 0;
 
+async function setCount() {
+  count += 1;
+
+  try {
+    let countString = String(count);
+    let countFile = await writeFile("count.txt", countString);
+
+    return countFile
+  } catch (error) {
+    console.log("Unable to update command invocation count", error);
+  }
+}
+
+try {
+  let countFile: Buffer = await readFile("count.txt");
+  let countNumber: number = Number(countFile.toString());
+
+  count = countNumber;
+} catch (error) {
+  console.log("Unable to load initial command invocation count", error);
+}
+
 client.on("message", (message: Message) => {
   if (message.content.match(/unzip/i) !== null) {
-    count += 1;
+    setCount();
 
     let randomImage: string =
       reactions[Math.floor(Math.random() * reactions.length)];
