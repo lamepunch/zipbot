0dee8357b68a1fb981f83974e8bc7d96f0653ef1
Spencer Summerville <me@grenuttag.com>
2021-07-30 06:01:27 -0400

    Add mostly complete leaderboard command

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,30 @@
-import { Client, Guild, Message, MessageEmbed } from "discord.js";
-import { PrismaClient } from "@prisma/client";
+import {
+  Client,
+  EmbedFieldData,
+  Guild,
+  Message,
+  MessageEmbed,
+} from "discord.js";
+import { PrismaClient, Server } from "@prisma/client";
 
 import reactions from "./reactions";
 
 let prisma: PrismaClient = new PrismaClient();
 let client: Client = new Client();
 
-let count: number = 0;
+let cachedLeaderboard: LeaderboardEntry[] = [];
+let lastCountFetch: Date | null = null;
+
+interface LeaderboardEntry {
+  position: number;
+  username: string;
+  invocations: number;
+}
 
 client.on("guildCreate", async (guild: Guild) => {
   // Whenever Zipbot joins a new server, create a server
   // entry in the database
-  let createServer = await prisma.server.create({
+  let createServer: Server = await prisma.server.create({
     data: {
       id: guild.id,
       name: guild.name,
@@ -20,13 +33,13 @@ client.on("guildCreate", async (guild: Guild) => {
 });
 
 client.on("message", async (message: Message) => {
-  let validMessage =
+  let isValidMessage: boolean =
     message.content.match(/unzip/i) !== null &&
-    message.author.bot !== true &&
+    message.author.bot === false &&
     message.channel.type === "text" &&
     message.guild !== null;
 
-  if (validMessage) {
+  if (isValidMessage) {
     let createInvocation = await prisma.invocation.create({
       data: {
         user: {
@@ -41,14 +54,15 @@ client.on("message", async (message: Message) => {
           },
         },
         server: {
-          connect: { id: message.guild.id },
+          connect: { id: message?.guild?.id },
         },
         channel: {
           connectOrCreate: {
             create: {
               id: message.channel.id,
+              // @ts-ignore
               name: message.channel.name,
-              server: { connect: { id: message.guild.id } },
+              server: { connect: { id: message?.guild?.id } },
             },
             where: {
               id: message.channel.id,
@@ -58,11 +72,9 @@ client.on("message", async (message: Message) => {
       },
     });
 
-    let invocationCount = await prisma.invocation.count({
-      where: { serverId: message.guild.id },
-    });
+    let invocationCount = createInvocation.id;
 
-    if (createInvocation && invocationCount) {
+    if (createInvocation) {
       let randomImage: string =
         reactions[Math.floor(Math.random() * reactions.length)];
 
@@ -71,6 +83,71 @@ client.on("message", async (message: Message) => {
         .setFooter(`#${invocationCount}`);
 
       message.channel.send(`${message.author} unzipped:`, response);
+    }
+  }
+
+  if (message.content === "!leaderboard") {
+    const THIRTY_MINUTES_IN_MILLISECONDS = 1800000;
+
+    function determineCacheState(): boolean {
+      if (lastCountFetch === null) {
+        return true;
+      } else {
+        let invokedAt = lastCountFetch.getTime();
+        let messageTime = message.createdAt.getTime();
+        let cooldownDelta = invokedAt - messageTime;
+
+        return cooldownDelta > THIRTY_MINUTES_IN_MILLISECONDS;
+      }
+    }
+
+    function constructEmbed(data: LeaderboardEntry[]): MessageEmbed {
+      // Convert the leaderboard data into an embed
+      let entries: EmbedFieldData[] = data
+        .slice(0, 10)
+        .map(({ position, username, invocations }: LeaderboardEntry) => ({
+          name: `${position}. ${username}`,
+          value: `${invocations} total unzips`,
+        }));
+
+      let response: MessageEmbed = new MessageEmbed()
+        .setTitle("Zipbot Leaderboard")
+        .addFields(entries)
+        .setFooter("Updated every 30 minutes");
+
+      return response;
+    }
+
+    function sendMessage(data: LeaderboardEntry[]) {
+      let response = constructEmbed(data);
+      message.channel.send(response);
+    }
+
+    let isCacheStale = determineCacheState();
+    if (isCacheStale) {
+      // Cache is stale, fetch invocation counts from the database,
+      let counts = await prisma.user.findMany({
+        orderBy: { invocations: { count: "desc" } },
+        include: { _count: true },
+      });
+
+      // Convert the results into an intermediate data structure
+      let leaderboard: LeaderboardEntry[] = counts.map((user, index) => ({
+        position: index + 1,
+        username: user.name,
+        invocations: user._count !== null ? user._count.invocations : 0,
+      }));
+
+      console.log("Caching leaderboard!");
+      // Cache the leaderboard
+      cachedLeaderboard = leaderboard;
+      lastCountFetch = new Date();
+
+      // Construct the embed and send it to the channel
+      sendMessage(leaderboard);
+    } else {
+      console.log("Retrieving cached leaderboard!");
+      sendMessage(cachedLeaderboard);
     }
   }
 });
