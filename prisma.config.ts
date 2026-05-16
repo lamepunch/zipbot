e73481ec9194a43d168c48ed8609c02a7fe483f9
grenuttag <me@grenuttag.com>
2026-05-16 12:58:51 -0400

    Use DIRECT_URL for CLI connections

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env("DIRECT_URL"),
   },
   migrations: {
     seed: "bun ./prisma/seed.ts",
